1e847e62bafdbf4794bd98c941a5c9173bf7b0f9
Rustem Shaydullin <ndiezel0@gmail.com>
2025-03-24 10:48:05 +0300

    Fix JSON key check in create_author.sh error response handling

--- scripts/dmt/create_author.sh
+++ scripts/dmt/create_author.sh
@@ -20,4 +20,4 @@ END
 )
 
 woorl -s "damsel/proto/domain_config_v2.thrift" "http://dmt:8022/v1/domain/author" AuthorManagement Create "${AUTHOR_PARAMS}" |
-jq -r 'if has("exists") then .exists.id else .id end'
+jq -r 'if has("already_exists") then .already_exists.id else .id end'
